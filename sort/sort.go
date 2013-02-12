931f854593085f32eb902a3bbcb5b1b20cedc651
Chen Yufei <cyfdecyf@gmail.com>
2013-02-12 22:38:00 +0800

    MergeSort algorithm.

--- sort/sort.go
+++ sort/sort.go
@@ -1,9 +1,13 @@
-// Package sort implements the following algorithms: selection sort, insertion sort, shell sort.
+// Package sort implements the following algorithms: selection sort, insertion sort, shell sort, merge sort.
+// For merge sort, I don't know what kind of interface is appropriate, so it only works on int slices.
 package sort
 
 import (
+	"fmt"
 	"sort"
 )
+
+var _ = fmt.Println
 
 // SelectionSort implements the selection algorithm.
 func SelectionSort(data sort.Interface) {
@@ -48,4 +52,40 @@ func ShellSort(data sort.Interface) {
 		}
 		h /= 3
 	}
+}
+
+// merge merges 2 parts of the sorted slices using the auxiliary slice.
+func merge(a, aux []int, lo, mid, hi int) {
+	copy(aux[lo:], a[lo:mid+1])
+	// reverse copy the other part, so don't need sentinel in merg
+	// refer to Algorithms, 3ed.
+	for i := mid + 1; i <= hi; i++ {
+		aux[hi+mid+1-i] = a[i]
+	}
+	i := lo
+	j := hi
+	for k := lo; k <= hi; k++ {
+		if aux[i] <= aux[j] {
+			a[k] = aux[i]
+			i++
+		} else {
+			a[k] = aux[j]
+			j--
+		}
+	}
+}
+
+func mergeSort(a, aux []int, lo, hi int) {
+	if hi <= lo {
+		return
+	}
+	mid := (hi-lo)/2 + lo
+	mergeSort(a, aux, lo, mid)
+	mergeSort(a, aux, mid+1, hi)
+	merge(a, aux, lo, mid, hi)
+}
+
+func MergeSort(a []int) {
+	aux := make([]int, len(a))
+	mergeSort(a, aux, 0, len(a)-1)
 }
--- sort/sort_test.go
+++ sort/sort_test.go
@@ -16,7 +16,7 @@ func init() {
 	data = make([][]int, nSeq, nSeq)
 
 	for i := 0; i < nSeq; i++ {
-		n := int(1<<uint32(i)) + rand.Rand(0, 10)
+		n := int(1<<uint32(i)) + rand.Rand(0, 5)
 		data[i] = rand.GenKRandomLessN(n, n)
 	}
 }
@@ -57,4 +57,46 @@ func TestInsertionSort(t *testing.T) {
 
 func TestShellSort(t *testing.T) {
 	testSortFunc(ShellSort, "ShellSort", t)
+}
+
+func TestMerge(t *testing.T) {
+	testData := []struct {
+		orig   []int
+		merged []int
+		lo     int
+		mid    int
+		hi     int
+	}{
+		{[]int{1, 0}, []int{0, 1}, 0, 0, 1},
+		{[]int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}, []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}, 0, 5, 9},
+		{[]int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}, []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}, 0, 4, 9},
+		{[]int{4, 5, 6, 7, 8, 9, 0, 1, 2, 3}, []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}, 0, 5, 9},
+		{[]int{4, 5, 6, 7, 8, 9, 0, 1, 2, 3}, []int{4, 5, 6, 0, 1, 7, 8, 9, 2, 3}, 3, 5, 7},
+		{[]int{9, 0, 1, 2, 3, 4, 5, 6, 7, 8}, []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}, 0, 0, 9},
+	}
+
+	aux := make([]int, 10)
+	for _, td := range testData {
+		cp := make([]int, 10)
+		copy(cp, td.orig)
+		merge(cp, aux, td.lo, td.mid, td.hi)
+		for i := td.lo; i < td.hi; i++ {
+			if cp[i] != td.merged[i] {
+				t.Fatalf("merge error for data %v lo=%d mid=%d hi=%d, got %v, should be %v\n",
+					td.orig, td.lo, td.mid, td.hi, cp, td.merged)
+			}
+		}
+	}
+}
+
+func TestMergeSort(t *testing.T) {
+	for i, d := range copyTestData() {
+		MergeSort(d)
+		for di, dv := range d {
+			if di != dv {
+				t.Fatalf("MergeSort wrong for the %dth data: %v, get: %v", i, data[i], d)
+			}
+		}
+	}
+
 }
